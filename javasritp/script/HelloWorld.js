9c997d8ddc75364a0c2ad9ea2ad40273a586174b
Agam <92200492+agamputra@users.noreply.github.com>
2023-06-12 09:59:39 +0700

    Add files via upload

---
+++ javasritp/script/HelloWorld.js
@@ -0,0 +1,27 @@
+document.writeln("Hello World");
+document.writeln("<br/>");
+document.writeln("NAMA : " + "  Agam ");
+document.write("<br/>");
+
+let fullName = "Ariesta Wicaksono Agam Putra";
+let firstName = "Ariesta Wicaksono";
+let middleName = "Agam";
+let lastName = "Putra";
+
+document.writeln("Nama Lengakap : " + fullName);
+document.write("<br/>");
+document.writeln("Nama Pertama : " + firstName);
+document.write("<br/>");
+document.writeln("Nama Tengah : " + middleName);
+document.write("<br/>");
+document.writeln("Nama Akhir : " + lastName);
+document.writeln("<br/>");
+
+let result = 1 + 2;
+document.writeln;
+
+let counter = 1;
+for (; counter <= 10; ) {
+  document.writeln("<p>Nomor" + counter + "</p>");
+  counter++;
+}
